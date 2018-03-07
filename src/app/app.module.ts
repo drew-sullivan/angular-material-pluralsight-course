11dd4de7f77a2dada4fff51cfeddb14cd619f676
dsullivan <drew.sullivan@hyland.com>
2018-03-07 12:44:41 -0500

    Adding styling and routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: 'demo', loadChildren: './demo/demo.module#DemoModule' }, // path to module and module name
+  { path: '**', redirectTo: 'demo'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
